044bc108e276094aff898358981b5220305a2b81
epatrizio <epatrizio@mpns.fr>
2024-02-14 09:33:09 +0100

    Add ocaml result monad example

--- design_patterns/monad.ml
+++ design_patterns/monad.ml
@@ -38,6 +38,29 @@ end = struct
   return (f a)
 end
 
+(* Result Monad - Same as Opt Monad with an error message *)
+module Res : sig
+  val return : 'a -> ('a, 'e) result
+
+  val bind : ('a, 'e) result -> ('a -> ('b, 'e) result) -> ('b, 'e) result
+
+  val ( let* ) : ('a, 'e) result -> ('a -> ('b, 'e) result) -> ('b, 'e) result
+
+  val lift : ('a -> 'b) -> ('a, 'e) result -> ('b, 'e) result
+end = struct
+  let return a =  Ok a
+
+  let bind ae f = match ae with
+    | Ok a -> f a
+    | Error e as err -> err
+
+  let ( let* ) = bind
+
+  let lift f ae = 
+    let* a = ae in
+    return (f a)
+end
+
 (* Choice monad - based on lists *)
 module Choice : sig
   val return : 'a -> 'a list
@@ -63,7 +86,7 @@ open Opt
 
 (* User inputs simulator *)
 let get_user_input : unit -> string option list =
-  fun () -> [ Some "42"; Some "str"; None ]
+  fun () -> [ Some "42"; Some "142"; Some "str"; None ]
 
 let test_user_input_between_zero_hundred (so : string option) : bool option =
   (* multi bind (with double Some/None pattern matching) is hidden *)
@@ -78,4 +101,30 @@ let () =
       match test_user_input_between_zero_hundred so with
       | Some true -> print_endline "OK"
       | _ -> print_endline "KO!" )
+    (get_user_input ())
+
+(* Minimal example with Res monad - error messages are better managed *)
+
+open Res
+
+let get_user_input : unit -> (string, string) Result.t list =
+  fun () -> [ Ok "42"; Ok "142"; Ok "str"; Error "User input error!" ]
+
+let test_user_input_integer (sr : (string, string) Result.t) : (int, string) Result.t =
+  let* s = sr in
+  let i = int_of_string_opt s in
+  match i with
+  | None -> Error "User input isn't an integer!"
+  | Some i -> Ok i
+
+let test_user_input_between_zero_hundred (sr : (string, string) Result.t) : (unit, string) Result.t =
+  let* i = test_user_input_integer sr in
+  if i > 0 && i < 100 then Ok () else Error "User input integer not between zero and hundred!"
+
+let () =
+  List.iter
+    (fun sr ->
+      match test_user_input_between_zero_hundred sr with
+      | Ok () -> print_endline "OK"
+      | Error e -> print_endline e )
     (get_user_input ())
